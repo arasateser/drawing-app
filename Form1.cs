ee4cb2ce0ba965baf29186eebe2ab0e96c2476ad
Aras Ateşer <43793683+balistis@users.noreply.github.com>
2021-03-04 22:02:36 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tatlibirdeneme
+{
+    public partial class ekran : Form
+    {
+        public ekran()
+        {
+            InitializeComponent();
+        }
+        Point[] Points = new Point[6];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //int count3ek = Convert.ToInt32(Math.Round(numericUpDown3.Value, 0));
+            //int count4ek = Convert.ToInt32(Math.Round(numericUpDown4.Value, 0));
+
+            //Size size = new Size(count1ek, count2ek);
+            //pictureBox1.Size = new Size(count3ek, count4ek); //alan buyuklkugunu belirleyecek ekran
+
+            sekilSec.Items.Add("Nokta");
+            sekilSec.Items.Add("Cizgi");
+            sekilSec.Items.Add("Poligon");
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sekilSec_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (sekilSec.Text == "Nokta")
+            {
+                turSec.Items.Clear();
+                turSec.Items.Add("Kucuk");
+                turSec.Items.Add("Orta");
+                turSec.Items.Add("Buyuk");
+            }
+            else if (sekilSec.Text == "Cizgi")
+            {
+                turSec.Items.Clear();
+                turSec.Items.Add("Ince");
+                turSec.Items.Add("Orta");
+                turSec.Items.Add("Kalin");
+            }
+            else if (sekilSec.Text == "Poligon")
+            {
+                turSec.Items.Clear();
+                turSec.Items.Add("Bos");
+                turSec.Items.Add("Tarali");
+                turSec.Items.Add("Dolu");
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buton1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        List<DrawAction> actions = new List<DrawAction>();
+
+        public class DrawAction
+        {
+            public char type { get; set; }             // this should be an Enum!
+            public Color color { get; set; }
+            public float penWidth { get; set; }        // only one of many Pen properties!
+            public List<Point> points { get; set; }    // use PointF for more precision
+
+            public DrawAction(char type_, Color color_, float penwidth_)
+            {
+                type = type_; color = color_; penWidth = penwidth_;
+                points = new List<Point>();
+            }
+        }
+
+        private void buton2_Click(object sender, EventArgs e)
+        {
+            int en = Convert.ToInt32(Math.Round(numericUpDown3.Value, 0));
+            int boy = Convert.ToInt32(Math.Round(numericUpDown4.Value, 0));
+            Size size = new Size(en, boy);
+            pictureBox1.Size = size;
+
+            if (sekilSec.Text == "Nokta")
+            {
+                if (turSec.Text == "Kucuk")
+                {
+                    int noktaKalin = 5;
+                    int count1ni = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2ni = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+
+                    actions.Add(new DrawAction('C', Color.Blue, 1));
+                    actions[0].points.Add(new Point(count1ni, count2ni));
+
+
+                    Bitmap pbk = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    using (Graphics goruntuKalin = Graphics.FromImage(pbk))
+                    {
+                        goruntuKalin.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                        goruntuKalin.FillEllipse(System.Drawing.Brushes.Cyan, count1ni, count2ni, noktaKalin, noktaKalin); //koortinatlar kullanicidan gelecek sekilde degisecek
+                    }
+                    pictureBox1.Image = pbk;
+                }
+                else if (turSec.Text == "Orta")
+                {
+                    int noktaKalin = 10;
+                    int count1no = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2no = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+
+                    actions.Add(new DrawAction('C', Color.Blue, 1));
+                    actions[0].points.Add(new Point(count1no, count2no));
+
+
+                    Bitmap pbk = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    using (Graphics goruntuKalin = Graphics.FromImage(pbk))
+                    {
+                        goruntuKalin.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                        goruntuKalin.FillEllipse(System.Drawing.Brushes.Cyan, count1no, count2no, noktaKalin, noktaKalin); //koortinatlar kullanicidan gelecek sekilde degisecek
+                    }
+                    pictureBox1.Image = pbk;
+                }
+                else if (turSec.Text == "Buyuk")
+                {
+                    int noktaKalin = 20;
+                    int count1nk = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2nk = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+
+                    actions.Add(new DrawAction('C', Color.Blue, 1));
+                    actions[0].points.Add(new Point(count1nk, count2nk));
+
+
+                    Bitmap pbk = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    using (Graphics goruntuKalin = Graphics.FromImage(pbk))
+                    {
+                        goruntuKalin.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                        goruntuKalin.FillEllipse(System.Drawing.Brushes.Cyan, count1nk, count2nk, noktaKalin, noktaKalin); //koortinatlar kullanicidan gelecek sekilde degisecek
+                    }
+                    pictureBox1.Image = pbk;
+                }
+            }
+
+
+            else if (sekilSec.Text == "Cizgi")
+            {
+                if (turSec.Text == "Ince")
+                {
+                    int count1ci = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2ci = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+
+                    actions.Add(new DrawAction('C', Color.Black, 1));
+                    actions[0].points.Add(new Point(count1ci, count2ci));
+
+                    Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                    using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+                    pictureBox1.Image = bmp;
+
+                }
+                else if (turSec.Text == "Orta")
+                {
+                    int count1co = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2co = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+                    actions.Add(new DrawAction('C', Color.Black, 3));
+                    actions[0].points.Add(new Point(count1co, count2co));
+
+                    Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                    using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+                    pictureBox1.Image = bmp;
+                }
+                else if (turSec.Text == "Kalin")
+                {
+                    int count1ck = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2ck = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+                    actions.Add(new DrawAction('C', Color.Black, 6));
+                    actions[0].points.Add(new Point(count1ck, count2ck));
+
+                    Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                    using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+                    pictureBox1.Image = bmp;
+                }
+            }
+            else if (sekilSec.Text == "Poligon")
+            {
+                if (turSec.Text == "Bos")
+                {
+                    int count1pb = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2pb = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+                    actions.Add(new DrawAction('P', Color.Red, 3));
+                    actions[0].points.Add(new Point(count1pb, count2pb));
+
+                    Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                    using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+                    pictureBox1.Image = bmp;
+                }
+                else if (turSec.Text == "Tarali")
+                {
+                    int count1pb = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2pb = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+                    actions.Add(new DrawAction('P', Color.Red, 3));
+                    actions[0].points.Add(new Point(count1pb, count2pb));
+
+
+                    Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                    using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+                    pictureBox1.Image = bmp;
+                }
+                else if (turSec.Text == "Dolu")
+                {
+                    int count1pb = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+                    int count2pb = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+                    actions.Add(new DrawAction('P', Color.Red, 3));
+                    actions[0].points.Add(new Point(count1pb, count2pb));
+                    using (SolidBrush br = new SolidBrush(Color.FromArgb(100, Color.Yellow)))
+                    {
+                        e.Graphics.FillPolygon(br, polyPoints.ToArray());
+                    }
+
+                    Bitmap bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                    using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        public void Draw(Graphics G, List<DrawAction> actions)
+        {
+            foreach (DrawAction da in actions)
+                if (da.type == 'N' && da.points.Count > 1)
+                    using (Pen pen = new Pen(da.color, da.penWidth))
+                        G.DrawLine(pen, da.points[0], da.points[1]);
+
+                else if (da.type == 'C' && da.points.Count > 1)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        using (Pen pen = new Pen(da.color, da.penWidth))
+                            G.DrawLines(pen, da.points.ToArray());
+                    }
+
+                }
+
+                else if (da.type == 'P' && da.points.Count > 1)
+                    using (Pen pen = new Pen(da.color, da.penWidth))
+                        G.DrawLines(pen, da.points.ToArray());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int count1ni = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            int count2ni = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+        }
+
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.DrawPolygon(Pens.Red, Points);
+        }
+
+        /*private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            int count1ek = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            int count2ek = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+
+            Size size = new Size(count1ek, count2ek);
+            pictureBox1.Size = size;
+
+        }*/
+    }
+}
